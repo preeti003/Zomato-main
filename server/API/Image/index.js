249a545b118f4ff00ce91aad447d6ea86ef21025
preeti003 <k.preeti1002@gmail.com>
2022-01-30 07:20:34 +0530

    changes

---
+++ server/API/Image/index.js
@@ -0,0 +1,80 @@
+// Libraries
+import express from "express";
+//import AWS from "aws-sdk";
+import multer from "multer";
+
+//Database Modal
+import { ImageModel } from "../../database/allModels";
+
+//upload to s3
+import { s3Upload } from "../../Utils/AWS/s3";
+
+const Router = express.Router();
+
+// multer config
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+/*
+Route     /
+Des       Get Image details
+Params    _id
+Access    Public
+Method    GET  
+*/
+Router.get("/:_id", async (req, res) => {
+  try {
+    const image = await ImageModel.findById(req.params._id);
+
+    return res.json({ image });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+//   AWS s3 buvket
+// const s3Bucket = new AWS.S3({
+//   accessKeyId: process.env.AWS_S3_ACCESS_KEY,
+//   secretAccessKey: process.env.AWS_S3_SECRET_KEY,
+//   region: "ap-south-1",
+// });
+
+
+/*
+Route           /image
+Des             Uploads given image to S3 bucket, and saves file link to mongodb
+Params          none
+Access          Public
+Method          POST
+*/
+Router.post("/", upload.single("file"), async (req, res) => {
+  try {
+    const file = req.file;
+
+    //s3 bucket options
+    const bucketOptions = {
+      Bucket: "shapeai-zomato-abcd",
+      Key: file.originalname,
+      Body: file.buffer,
+      ContentType: file.mimetype,
+      ACL: "public-read", //Access Control List
+    };
+
+    const uploadImage = await s3Upload(bucketOptions);
+
+    return res.status(200).json({ uploadImage });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+//   const s3Upload = (options) => {
+//   return new Promise((resolve, reject) =>
+//       s3Bucket.upload(options, (error, data) => {
+//           if (error) return reject(error);
+//           return resolve(data);
+//       })
+//   );
+// };
+
+export default Router;
